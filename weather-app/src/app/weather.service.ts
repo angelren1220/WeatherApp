fd2a11e003ec91d69285bb82ad54553647b6019f
Linxue <angelren1220@gmail.com>
2023-09-19 03:03:41 -0400

    add httpClient

---
+++ weather-app/src/app/weather.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environments';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  private apiUrl = 'https://api.weatherapi.com/v1/current.json';
+  private apiKey = environment.apiKey;
+
+  constructor(private http: HttpClient){}
+
+  getWeather(city: String) {
+    return this.http.get(`${this.apiUrl}?key=${this.apiKey}&q=${city}`)
+  }
+}
---
+++ weather-app/src/environments/environments.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiKey: '8638b628106b4a3ab9852925231909',
+};
