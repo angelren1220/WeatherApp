b8a3459dc4609a35930984418b2c724f7d7e5765
Linxue <angelren1220@gmail.com>
2023-09-25 04:26:55 -0400

    remove pagination

--- src/app/search-history/search-history.component.ts
+++ src/app/search-history/search-history.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, SimpleChanges } from '@angular/core';
 import { WeatherService } from '../weather.service';
 
 @Component({
@@ -11,50 +11,25 @@ export class SearchHistoryComponent {
   @Input() searchHistory: { city: string, weatherData: any, forecastData: any }[] = [];
   @Output() citySelected = new EventEmitter<string>();
 
-  // Pagination properties
-  currentPage: number = 1;
-  itemsPerPage: number = 5;  
-  totalPages: number;
   city: string = '';
   weatherData: any;
   forecastData: any;
 
-  constructor(public weatherService: WeatherService) {
-    // Calculate total pages based on itemsPerPage
-    this.totalPages = Math.ceil(this.searchHistory.length / this.itemsPerPage);
-  }
+  constructor(public weatherService: WeatherService) {}
 
-  ngOnInit() {
-    // Load data for the first page
-    this.loadDataFromPage(0);
-  }
-
-
-  // Add methods to handle pagination
-
-  goToNextPage() {
-    if (this.currentPage < this.totalPages) {
-      this.currentPage++;
-      this.loadDataFromPage(this.currentPage - 1); // Arrays are 0-indexed
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['searchHistory']) {
+      this.loadDataFromMostRecent();
     }
   }
 
-  goToPreviousPage() {
-    if (this.currentPage > 1) {
-      this.currentPage--;
-      this.loadDataFromPage(this.currentPage - 1);
+  loadDataFromMostRecent() {
+    const recentData = this.searchHistory[this.searchHistory.length - 1]; 
+    if (recentData) {
+      this.city = recentData.city;
+      this.weatherData = recentData.weatherData;
+      this.forecastData = recentData.forecastData;
     }
   }
-
-
-  loadDataFromPage(pageIndex: number) {
-    const selectedData = this.searchHistory[pageIndex];
-    if (selectedData) {
-      this.city = selectedData.city;
-      this.weatherData = selectedData.weatherData;
-      this.forecastData = selectedData.forecastData;
-    }
-  }
-
 }
 
